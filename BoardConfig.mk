4b93ebbad457ea2ed05ab9e3c3c1b236d8e0b903
Brandon Bennett <bennetb@gmail.com>
2012-01-12 14:49:13 -0700

    Initial CM9 support
    Starts adding in nessisary files to boot CM9.
    
    Note: Graphics will work only after you use the default hw.composer.  The Gingerbread one will not
    work with ICS.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,25 +23,45 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/bn/acclaim/BoardConfigVendor.mk
 
+# Processor 
 TARGET_BOARD_PLATFORM := omap4
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
 TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
-TARGET_BOOTLOADER_BOARD_NAME := omap4
+TARGET_BOOTLOADER_BOARD_NAME := acclaim
 
-BOARD_KERNEL_CMDLINE := androidboot.console=ttyO0 console=ttyO0,115200n8 mem=448M@0x80000000 mem=512M@0xA0000000 init=/init rootwait vram=32M,82000000 omapfb.vram=0:5M@82000000
+# Kernel/Boot
+#BOARD_KERNEL_CMDLINE := androidboot.console=ttyO0 console=ttyO0,115200n8 mem=448M@0x80000000 mem=512M@0xA0000000 init=/init rootwait vram=32M,82000000 omapfb.vram=0:5M@82000000
+BOARD_KERNEL_CMDLINE := androidboot.console=ttyO0 console=ttyO0,115200n8 mem=448M@0x80000000 mem=512M@0xA0000000 init=/init rootwait vram=32M omapfb.vram=0:16M
 BOARD_KERNEL_BASE := 0x80080000
 BOARD_KERNEL_PAGESIZE := 4096
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
 
-# fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00280000)
-BOARD_RECOVERYIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00500000)
-BOARD_SYSTEMIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x07500000)
-BOARD_USERDATAIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x04ac0000)
-
-# The size of a block that can be marked bad.
+# Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+#BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 15728640
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 641728512
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12949893120
 BOARD_FLASH_BLOCK_SIZE := 131072
+
+# Graphics
+BOARD_EGL_CFG := device/bn/acclaim/egl.cfg
+COMMON_GLOBAL_CFLAGS += -DMISSING_EGL_EXTERNAL_IMAGE \
+			-DMISSING_EGL_PIXEL_FORMAT_YV12 \
+			-DMISSING_GRALLOC_BUFFERS
+# Workaround for eglconfig error
+BOARD_NO_RGBX_8888 := true
+#USE_OPENGL_RENDERER := true
+OMAP_ENHANCEMENT := true
+#ENHANCED_DOMX := true
+#BLTSVILLE_ENHANCEMENT :=true
+ifdef OMAP_ENHANCEMENT
+COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT -DTARGET_OMAP4
+endif
 
 # Recovery
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/bn/acclaim/recovery/recovery_ui.c
--- device.mk
+++ device.mk
@@ -1,9 +1,26 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+#
+# Copyright (C) 2011 The Android Open-Source Project
+# Copyright (C) 2011 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-$(call inherit-product-if-exists, vendor/bn/acclaim/acclaim-vendor.mk)
+# This file includes all definitions that apply to ALL tuna devices, and
+# are also specific to tuna devices
+#
+# Everything in this directory will become public
 
 DEVICE_PACKAGE_OVERLAYS += device/bn/acclaim/overlay
-
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := device/bn/acclaim/kernel
@@ -11,11 +28,59 @@ else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
-    
-$(call inherit-product, build/target/product/full.mk)
+PRODUCT_COPY_FILES := \
+    $(LOCAL_KERNEL):kernel \
+    device/bn/acclaim/init.acclaim.rc:root/init.acclaim.rc \
+    device/bn/acclaim/init.acclaim.usb.rc:root/init.acclaim.usb.rc \
+    device/bn/acclaim/ueventd.acclaim.rc:root/ueventd.acclaim.rc
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_acclaim
-PRODUCT_DEVICE := acclaim
+# Graphics
+PRODUCT_COPY_FILES += \
+	device/bn/acclaim/prebuilt/imgtec/vendor/lib/egl/libEGL_POWERVR_SGX540_120.so:system/vendor/lib/egl/libEGL_POWERVR_SGX540_120.so \
+	device/bn/acclaim/prebuilt/imgtec/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so:system/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so \
+	device/bn/acclaim/prebuilt/imgtec/vendor/lib/egl/libGLESv2_POWERVR_SGX540_120.so:system/vendor/lib/egl/libGLESv2_POWERVR_SGX540_120.so \
+	device/bn/acclaim/prebuilt/imgtec/vendor/lib/hw/gralloc.omap4.so:system/vendor/lib/hw/gralloc.omap4.so \
+	device/bn/acclaim/prebuilt/imgtec/vendor/lib/libIMGegl.so:system/vendor/lib/libIMGegl.so \
+	device/bn/acclaim/prebuilt/imgtec/vendor/lib/libPVRScopeServices.so:system/vendor/lib/libPVRScopeServices.so \
+	device/bn/acclaim/prebuilt/imgtec/vendor/lib/libglslcompiler.so:system/vendor/lib/libglslcompiler.so \
+	device/bn/acclaim/prebuilt/imgtec/vendor/lib/libpvr2d.so:system/vendor/lib/libpvr2d.so \
+	device/bn/acclaim/prebuilt/imgtec/vendor/lib/libpvrANDROID_WSEGL.so:system/vendor/lib/libpvrANDROID_WSEGL.so \
+	device/bn/acclaim/prebuilt/imgtec/vendor/lib/libsrv_init.so:system/vendor/lib/libsrv_init.so \
+	device/bn/acclaim/prebuilt/imgtec/vendor/lib/libsrv_um.so:system/vendor/lib/libsrv_um.so \
+	device/bn/acclaim/prebuilt/imgtec/vendor/lib/libusc.so:system/vendor/lib/libusc.so \
+	device/bn/acclaim/prebuilt/imgtec/bin/pvrsrvinit:system/vendor/bin/pvrsrvinit 
+
+
+
+# Place permission files
+PRODUCT_COPY_FILES += \
+	frameworks/base/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+	frameworks/base/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+	frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+	frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+	frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+	frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+	frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+	frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+	frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+
+PRODUCT_PACKAGES += \
+    hwcomposer.default
+
+PRODUCT_CHARACTERISTICS := tablet
+
+# Screen size is "large", density is "mdpi"
+PRODUCT_AAPT_CONFIG := large mdpi
+
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+$(call inherit-product, frameworks/base/build/tablet-dalvik-heap.mk)
+$(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
+#$(call inherit-product-if-exists, vendor/ti/proprietary/omap4/ti-omap4-vendor.mk)
+
+$(call inherit-product-if-exists, vendor/bn/acclaim/device-vendor.mk)
+$(call inherit-product-if-exists, vendor/bn/acclaim/device-vendor-blobs.mk)
