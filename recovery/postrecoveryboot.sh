d14c75d46d33c43846fc8f09a79dbd8cf5da0bd1
chrmhoffmann <chrmhoffmann@gmail.com>
2012-03-17 20:15:25 +0100

    attempt to fix mounting of more than one usb mass storage partition boot to recovery (but not bootloader) nandroid backup align mem to 4M (seems to fix crashes when copying large files)

---
+++ recovery/postrecoveryboot.sh
@@ -0,0 +1,11 @@
+#!/sbin/sh
+
+# Resets the boot counter and the BCB instructions
+mkdir /bootdata
+mount /dev/block/mmcblk0p6 /bootdata
+
+# Zero out the boot counter
+dd if=/dev/zero of=/bootdata/BootCnt bs=1 count=4
+
+# Reset the bootloader control block (bcb) file
+dd if=/dev/zero of=/bootdata/BCB bs=1 count=1088
