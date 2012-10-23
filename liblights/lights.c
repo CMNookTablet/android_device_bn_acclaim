fa1f943fd272ae9c202a2f993fb327f2904e00d3
chrmhoffmann <chrmhoffmann@gmail.com>
2012-10-23 21:25:07 +0200

    lights: remove dead code and don't update if value doesn't change.

--- liblights/lights.c
+++ liblights/lights.c
@@ -29,31 +29,14 @@ static pthread_once_t g_init = PTHREAD_ONCE_INIT;
 static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
 
 char const *const LCD_FILE = "/sys/class/leds/lcd-backlight/brightness";
-//char const *const ORANGE_LED_FILE = "/sys/class/leds/led-green";
-//char const *const GREEN_LED_FILE = "/sys/class/leds/led-orange";
-//char const *const LED_FILE = "/dev/an30259a_leds";
 
-#define IMAX 0 // 12.75mA power consumption
-
-// Slope values, based on total blink of 1000ms
-#define SLOPE_UP_1		450
-#define SLOPE_UP_2		(500-SLOPE_UP_1)
-#define SLOPE_DOWN_1	SLOPE_UP_2
-#define SLOPE_DOWN_2	SLOPE_UP_1
-// brightness at mid-slope, on 0 - 127 scale
-#define MID_BRIGHTNESS  31
-
-void init_g_lock(void)
-{
+void init_g_lock(void) {
 	pthread_mutex_init(&g_lock, NULL);
 }
 
-static int write_int(char const *path, int value)
-{
+static int write_int(char const *path, int value) {
 	int fd;
-	static int already_warned;
-
-	already_warned = 0;
+	static int already_warned = 0;
 
 	ALOGV("write_int: path %s, value %d", path, value);
 	fd = open(path, O_RDWR);
@@ -73,29 +56,37 @@ static int write_int(char const *path, int value)
 	}
 }
 
-static int rgb_to_brightness(struct light_state_t const *state)
-{
+static int rgb_to_brightness(struct light_state_t const *state) {
 	int color = state->color & 0x00ffffff;
 
 	return ((77*((color>>16) & 0x00ff))
 		+ (150*((color>>8) & 0x00ff)) + (29*(color & 0x00ff))) >> 8;
 }
 
+/* Previous value of brightness */
+static int brightness_prev_value = -1;
+
 static int set_light_backlight(struct light_device_t *dev,
-			struct light_state_t const *state)
-{
+			       struct light_state_t const *state) {
 	int err = 0;
 	int brightness = rgb_to_brightness(state);
 
+	if (brightness == brightness_prev_value) {
+		/* No need to set same value twice */
+		return err;
+	}
+
 	pthread_mutex_lock(&g_lock);
 	err = write_int(LCD_FILE, brightness);
-
 	pthread_mutex_unlock(&g_lock);
+
+	if (! err) {
+		brightness_prev_value = brightness;
+	}
 	return err;
 }
 
-static int close_lights(struct light_device_t *dev)
-{
+static int close_lights(struct light_device_t *dev) {
 	ALOGV("close_light is called");
 	if (dev)
 		free(dev);
@@ -103,68 +94,13 @@ static int close_lights(struct light_device_t *dev)
 	return 0;
 }
 
-/* LEDs */
-/*static int write_leds()
-{
-	int err = 0;
-	int imax = IMAX;
-	int fd;
-
-	pthread_mutex_lock(&g_lock);
-
-	fd = open(LED_FILE, O_RDWR);
-	if (fd >= 0) {
-		close(fd);
-	} else {
-		LOGE("failed to open %s!", LED_FILE);
-		err =  -errno;
-	}
-
-	pthread_mutex_unlock(&g_lock);
-
-	return err;
-}
-
-static int set_light_leds(struct light_state_t const *state, int type)
-{
-
-	switch (state->flashMode) {
-	case LIGHT_FLASH_NONE:
-		break;
-	case LIGHT_FLASH_TIMED:
-	case LIGHT_FLASH_HARDWARE:
-		break;
-	default:
-		return -EINVAL;
-	}
-
-	return 0;
-}*/
-
-/*static int set_light_leds_notifications(struct light_device_t *dev,
-			struct light_state_t const *state)
-{
-	return set_light_leds(state, 0);
-}
-
-static int set_light_leds_attention(struct light_device_t *dev,
-			struct light_state_t const *state)
-{
-	return set_light_leds(state, 1);
-}*/
-
 static int open_lights(const struct hw_module_t *module, char const *name,
-						struct hw_device_t **device)
-{
+		       struct hw_device_t **device) {
 	int (*set_light)(struct light_device_t *dev,
-		struct light_state_t const *state);
+			 struct light_state_t const *state);
 
 	if (0 == strcmp(LIGHT_ID_BACKLIGHT, name))
 		set_light = set_light_backlight;
-/*	else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name))
-		set_light = set_light_leds_notifications;
-	else if (0 == strcmp(LIGHT_ID_ATTENTION, name))
-		set_light = set_light_leds_attention;*/
 	else
 		return -EINVAL;
 
@@ -185,7 +121,7 @@ static int open_lights(const struct hw_module_t *module, char const *name,
 }
 
 static struct hw_module_methods_t lights_module_methods = {
-	.open =  open_lights,
+	.open = open_lights,
 };
 
 struct hw_module_t HAL_MODULE_INFO_SYM = {
@@ -193,7 +129,7 @@ struct hw_module_t HAL_MODULE_INFO_SYM = {
 	.version_major = 1,
 	.version_minor = 0,
 	.id = LIGHTS_HARDWARE_MODULE_ID,
-	.name = "lights Module",
+	.name = "TI OMAP lights Module",
 	.author = "Google, Inc.",
 	.methods = &lights_module_methods,
 };
