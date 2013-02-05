8385ba70b219de1c403c626411206d941ffcc205
Kuzemko Aleksandr <kuzemkoa@gmail.com>
2013-02-05 17:40:57 +0000

    FixMAC script refactoring

--- prebuilt/bin/fix-mac.sh
+++ prebuilt/bin/fix-mac.sh
@@ -1,12 +1,17 @@
 #!/system/bin/sh
 
+
 PATH=/sbin:/vendor/bin:/system/sbin:/system/bin:/system/xbin
+ORIG_NVS_BIN=/system/etc/firmware/ti-connectivity/wl1271-nvs_127x.bin
 NVS_BIN=/system/etc/firmware/ti-connectivity/wl1271-nvs.bin
 
-if [ ! -f "$NVS_BIN" ]; then
 busybox mount -o remount,rw /system
-    cp ${NVS_BIN}.orig ${NVS_BIN}
-    MAC=`cat /rom/devconf/MACAddress | sed 's/\(..\)\(..\)\(..\)\(..\)\(..\)/\1:\2:\3:\4:\5:/'`
-    calibrator set nvs_mac $NVS_BIN $MAC
-    busybox mount -o remount,ro /system
+
+if busybox [ ! -f "$NVS_BIN" ]; then
+    cp ${ORIG_NVS_BIN} ${NVS_BIN}
 fi
+
+MAC=`cat /rom/devconf/MACAddress | sed 's/\(..\)\(..\)\(..\)\(..\)\(..\)/\1:\2:\3:\4:\5:/'`
+calibrator set nvs_mac $NVS_BIN $MAC
+
+busybox mount -o remount,ro /system
