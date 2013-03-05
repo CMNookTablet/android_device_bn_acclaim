2a39017d974d305bdb22e155192164ca882dfc69
chrmhoffmann <chrmhoffmann@gmail.com>
2013-03-06 00:51:18 +0100

    cm-10.1: prop extraction fixed

---
+++ extract-files.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+VENDOR=bn
+DEVICE=acclaim
+
+BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+
+echo "Pulling $DEVICE files..."
+for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
+DIR=`dirname $FILE`
+    if [ ! -d $BASE/$DIR ]; then
+mkdir -p $BASE/$DIR
+    fi
+adb pull /system/$FILE $BASE/$FILE
+done
+
+./setup-makefiles.sh
+
+# Call up to omap4-common
+../omap4-common/extract-files.sh
